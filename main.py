0b5113e02aa74cdd88d16962c65eae459dd70e4b
Pedro <pedrod33@ua.pt>
2021-11-03 19:11:55 +0000

    fixed

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from collections import namedtuple
 import string
 import random
 from getch import getch 
-from time import time
+from time import time, sleep
 #####################################
 
 #Inputs:
@@ -20,7 +20,7 @@ from time import time
 #Função começar teste:
 #Caso utilizador pretenda começar o teste pressiona uma tecla
 #Caso utilizador pretenda sair, pressiona X
-def start_test():
+def can_start_test():
 
     print(Fore.RED + Style.BRIGHT + 'Typing test: ' +  Style.RESET_ALL + Fore.BLUE + Style.BRIGHT +  '\nPress a key to start the test' +  Style.RESET_ALL + '(Space to Leave)'  +  Style.RESET_ALL)
     pressed_key = ord(getch())
@@ -29,11 +29,11 @@ def start_test():
     if pressed_key == 32:
         pressed_key = ('Space Bar')      
         print( 'You typed ' + Fore.CYAN + Style.BRIGHT + pressed_key + Style.RESET_ALL + Fore.RED + Style.BRIGHT +'\nTerminating...')
-        time.sleep(1)
+        sleep(1)
         return False
     else:
         print(Fore.GREEN + Style.BRIGHT + 'Starting test... ' + Style.RESET_ALL)
-        time.sleep(1)
+        sleep(1)
         return True
 
 ######## Funcao para obter caracter aleatorio ########
@@ -53,28 +53,32 @@ def read_key():
     print (pressed_keys)
 
 #Main:
-def timed_mode(time):
+def timed_mode(max_time):
+    print(max_time)
+    print(Fore.GREEN + Style.BRIGHT + 'Test started' + Style.RESET_ALL)
+    start_time=time()
+    print(start_time)
+    generate_rkey()
+    read_key()
 
-def main():
-    parser = argparse.ArgumentParser(description='PSR argparse example.')
-    parser.add_argument('-utm', '--use_time_mode', action='store_true', help='')
-    parser.add_argument('-mn', '--max_number', type=int, help='')
-    args = vars(parser.parse_args())
-    print(args)
-
-    if args['use_time_mode']:
-        print('Using time mode. test will run up to ' + str(args['max_number']) + ' seconds')
-        timed_mode(args['max_number'])
-    else:
-        print('Not using time mode. Test will ask for ' + str(args['max_number']) + ' responses')
-    print(args['use_time_mode'])
-    if args['use_time_mode'] and args['max_number'] > 0 :
-        elif not args['use_time_mode'] and args['max_number'] > 0 :
-        max_key_mode(args['max_number'])
-    if start_test():
+def max_key_mode(num_chars):
+    for i in range(num_chars):
         print(Fore.GREEN + Style.BRIGHT + 'Test started' + Style.RESET_ALL)
         generate_rkey()
         read_key()
+def main():
+    parser = argparse.ArgumentParser(description='Typing test')
+    parser.add_argument('-utm', '--use_time_mode', action='store_true', help='Turns the test from a maximum input test to a test with limited time')
+    parser.add_argument('-mn', '--max_number', type=int, help='Max number of seconds if in time mode or maximum number of inputs for number of inputs mode')
+    args = vars(parser.parse_args())
+
+    if can_start_test():
+        if args['use_time_mode']:
+            print('Using time mode. test will run up to ' + str(args['max_number']) + ' seconds')
+            timed_mode(args['max_number'])
+        else:
+            print('Not using time mode. Test will ask for ' + str(args['max_number']) + ' responses')
+            max_key_mode(args['max_number'])
 
     else:
         
